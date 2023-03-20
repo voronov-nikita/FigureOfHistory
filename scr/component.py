6abda884e053a9f669edecd0ef2db0f053ccef6f
voronov-nikita <114691383+voronov-nikita@users.noreply.github.com>
2023-03-20 22:34:57 +0300

    no_work_collider

--- scr/component.py
+++ scr/component.py
@@ -1,15 +1,23 @@
+from random import randint
+import pygame as pg
+
+
+def new_random_coords(edges):
+    return randint(edges, 500 - edges)
+
+
 class Standartindivid():
 
     def move_right(self, cord_x, size_individ):
         if cord_x < 500 - size_individ - 5:
             return True
         return False
-    
+
     def move_left(self, cord_x):
         if cord_x > 0:
             return True
         return False
-    
+
     def move_up(self, cord_y):
         if cord_y > 1:
             return True
@@ -19,6 +27,20 @@ class Standartindivid():
         if cord_y < 500 - size_individ - 5:
             return True
         return False
-    
 
 
+class GreenEat():
+    def __init__(self):
+        self.color = (0, 255, 0)
+        self.size_object = 10
+
+        self.rect = pg.Rect(0, 0, 100, 100)
+        self.cord_x = new_random_coords(self.size_object)
+        self.cord_y = new_random_coords(self.size_object)
+
+    def create_food(self, screen):
+        position = (self.cord_x,
+                    self.cord_y,
+                    self.size_object,
+                    self.size_object)
+        pg.draw.rect(screen, self.color, position)
--- scr/main.py
+++ scr/main.py
@@ -1,54 +1,43 @@
 import pygame as pg
 from random import randint
 
-from component import Standartindivid
+from component import Standartindivid, GreenEat
 
 
 class OpenSpace():
     def __init__(self):
         self.background_colour = (255, 255, 255)
         self.screen = pg.display.set_mode((500, 500))
-        pg.display.set_caption("Generation")
         self.FPS = 30
         self.clock = pg.time.Clock()
         self.work = True
 
 
 class Individ(Standartindivid):
-    def __init__(self):
+    def __init__(self, window):
         self.size_individ = 10
 
         self.cord_x = randint(0, 500)
         self.cord_y = randint(0, 500)
 
         self.life = True
+        self.rect = pg.Rect(*window.get_rect().center, 0, 0).inflate(
+            self.size_individ*20, self.size_individ*20)
 
         self.color = (255, 0, 0)
-        self.position = (self.cord_x, 
-                         self.cord_y, 
-                         self.size_individ, 
+        self.position = (self.cord_x,
+                         self.cord_y,
+                         self.size_individ,
                          self.size_individ)
 
     def draw_hero(self, screen):
         pg.draw.rect(screen, self.color, self.position)
 
-    def update_position(self, new_pos:tuple):
+    def update_position(self, new_pos: tuple):
         self.position = new_pos
 
 
-
-place = OpenSpace()
-hero = Individ()
-
-while place.work:
-    place.clock.tick(place.FPS)
-     
-    # проверка на закрытие экрана
-    for event in pg.event.get():
-        if event.type == pg.QUIT:
-            place.work = False
-
-    # список нажатых клавиш
+def move_object_hero():
     keys_button = pg.key.get_pressed()
 
     if keys_button[pg.K_LEFT] and hero.move_left(hero.cord_x):
@@ -60,17 +49,49 @@ while place.work:
     if keys_button[pg.K_DOWN] and hero.move_down(hero.cord_y, hero.size_individ):
         hero.cord_y += 10
 
-    pos = ( hero.cord_x,
-            hero.cord_y,
-            hero.size_individ,
-            hero.size_individ)
 
+def intersection_elems():
+    for eats in list_object_eat:
+        if hero.rect.colliderect(eat.rect):
+            print("kill")
+
+
+pg.display.set_caption("Generation")
+place = OpenSpace()
+hero = Individ(place.screen)
+
+# создаем amount_eat экземпляров еды для индивидума
+amount_food = 50
+list_object_eat = []
+for i in range(amount_food):
+    eat = GreenEat()
+    list_object_eat.append(eat)
+
+
+while place.work:
+    place.clock.tick(place.FPS)
+
+    # проверка на закрытие экрана
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            place.work = False
+    # движение
+    move_object_hero()
+    intersection_elems()
+
+    pos = (hero.cord_x,
+           hero.cord_y,
+           hero.size_individ,
+           hero.size_individ)
+
+#   <------------- Обработать все изображения -------------->
     hero.update_position(pos)
     place.screen.fill(place.background_colour)
-    # отрисовка героя
+    # отрисовка
+    for elem in list_object_eat:
+        elem.create_food(place.screen)
     hero.draw_hero(place.screen)
-    
+
     pg.display.update()
 
 pg.quit()
-
