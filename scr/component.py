c026cda06c3d9dc90b9a0208e1cf14b4cc122f7d
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-03-28 22:01:48 +0300

    fix_problem_with_cord-position join_the_code

--- scr/component.py
+++ scr/component.py
@@ -6,7 +6,38 @@ def new_random_coords(edges):
     return randint(edges, 500 - edges)
 
 
-class Standartindivid(pg.Rect):
+class StandartIndivid(pg.Rect):
+
+    def __init__(self):
+        super().__init__(self)
+        self.size_individ = 10
+        self.speed = 5
+        self.point = 0
+
+        self.cord_x = randint(0, 500)
+        self.cord_y = randint(0, 500)
+        self.count_point = 0
+
+        self.life = True
+
+        self.surface = pg.Surface((self.size_individ, self.size_individ))
+        self.rect = self.surface.get_rect()
+
+        self.color = (255, 0, 0)
+        self.position = (self.cord_x,
+                         self.cord_y,
+                         self.size_individ,
+                         self.size_individ)
+
+    def draw_hero(self, screen):
+        pg.draw.rect(screen, self.color, self.position)
+        self.surface.fill(self.color)
+        self.rect.x = self.cord_x
+        self.rect.y = self.cord_y
+        screen.blit(self.surface, self.rect)
+
+    def update_position(self, new_pos: tuple):
+        self.position = new_pos
 
     def move_right(self, cord_x, size_individ):
         if cord_x < 500 - size_individ - 5:
@@ -27,6 +58,28 @@ class Standartindivid(pg.Rect):
         if cord_y < 500 - size_individ - 5:
             return True
         return False
+    
+    def move_object_hero(self, list_cord: list):
+        try:
+            elem = list_cord[self.count_point]
+
+            if self.cord_x >= elem[0] and self.move_left(self.cord_x):
+                self.cord_x -= self.speed
+            elif self.cord_x <= elem[0] and self.move_right(self.cord_x, self.size_individ):
+                self.cord_x += self.speed
+
+            if self.cord_y >= elem[1] and self.move_up(self.cord_y):
+                self.cord_y -= self.speed
+            elif self.cord_y <= elem[1] and self.move_down(self.cord_y, self.size_individ):
+                self.cord_y += self.speed
+
+            if elem[0] in range(self.cord_x-self.size_individ, self.cord_x+self.size_individ//2) and elem[1] in range(self.cord_y-self.size_individ//2, self.cord_y+self.size_individ//2):
+                self.count_point += 1
+
+                print(self.point)
+
+        except IndexError:
+            pass
 
 
 class GreenEat(pg.Rect):
--- scr/main.py
+++ scr/main.py
@@ -1,7 +1,7 @@
 import pygame as pg
 from random import randint
 
-from component import Standartindivid, GreenEat
+from component import StandartIndivid, GreenEat
 
 pg.init()
 
@@ -15,71 +15,15 @@ class OpenSpace():
         self.work = True
 
 
-class Individ(Standartindivid):
-    def __init__(self, window):
-        super().__init__(self)
-        self.size_individ = 10
-        self.speed = 10
-        self.point = -50
-
-        self.cord_x = randint(0, 500)
-        self.cord_y = randint(0, 500)
-        self.count_point = 0
-
-        self.life = True
-
-        self.surface = pg.Surface((self.size_individ, self.size_individ))
-        self.rect = self.surface.get_rect()
-
-        self.color = (255, 0, 0)
-        self.position = (self.cord_x,
-                         self.cord_y,
-                         self.size_individ,
-                         self.size_individ)
-
-    def draw_hero(self, screen):
-        pg.draw.rect(screen, self.color, self.position)
-        self.surface.fill(self.color)
-        self.rect.x = self.cord_x
-        self.rect.y = self.cord_y
-        screen.blit(self.surface, self.rect)
-
-    def update_position(self, new_pos: tuple):
-        self.position = new_pos
-
-
 pg.display.set_caption("Generation")
 
 
 def start():
-    def move_object_hero(list_cord: list, pos: tuple):
-        try:
-            elem = list_cord[hero.count_point]
-
-            if hero.cord_x >= elem[0] and hero.move_left(hero.cord_x):
-                hero.cord_x -= hero.speed
-            elif hero.cord_x <= elem[0] and hero.move_right(hero.cord_x, hero.size_individ):
-                hero.cord_x += hero.speed
-
-            if hero.cord_y >= elem[1] and hero.move_up(hero.cord_y):
-                hero.cord_y -= hero.speed
-            elif hero.cord_y <= elem[1] and hero.move_down(hero.cord_y, hero.size_individ):
-                hero.cord_y += hero.speed
-
-            if elem[0] in range(hero.cord_x-hero.size_individ-1, hero.cord_x+hero.size_individ+1) and elem[1] in range(hero.cord_y-hero.size_individ, hero.cord_y+hero.size_individ+1):
-                print("+", list_cord_eats)
-                hero.count_point += 1
-
-                print(hero.point)
-
-        except IndexError:
-            pass
 
     def intersection_elems():
         for eats in list_object_eat:
             if hero.rect.colliderect(eats.rect):
                 if eats.generation:
-                    print("OK\n")
                     eats.generation = False
                 else:
                     hero.point += 1
@@ -96,15 +40,14 @@ def start():
         pg.display.update()
 
     place = OpenSpace()
-    hero = Individ(place.screen)
+    hero = StandartIndivid()
 
     # создаем amount_eat экземпляров еды для индивидума
     # для обозначения точного количесвта очков был взят параметр для противоположного от количества очков героя
-    amount_food = -hero.point
+    amount_food = 50
     list_object_eat = [GreenEat(place.screen) for _ in range(amount_food)]
 
-    list_cord_eats = [(elem.cord_x, elem.cord_y) for elem in list_object_eat]
-    list_cord_eats = sorted(list_cord_eats)
+    list_cord_eats = sorted([(elem.cord_x, elem.cord_y) for elem in list_object_eat])
     while place.work:
         place.clock.tick(place.FPS)
 
@@ -118,7 +61,7 @@ def start():
                hero.cord_y,
                hero.size_individ,
                hero.size_individ)
-        move_object_hero(list_cord_eats, pos)
+        hero.move_object_hero(list_cord_eats)
         intersection_elems()
 
         #   <------------- Обработать все изображения -------------->
