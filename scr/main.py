f5433ee10ef82bef44829d79484b36ea5823834b
voronov <114691383+voronov-nikita@users.noreply.github.com>
2023-08-23 01:01:41 +0300

    new-update

--- scr/main.py
+++ scr/main.py
@@ -1,11 +1,14 @@
 import pygame as pg
+import random
 import sys
+
+
+LIST_COLORS:list[str] = ["red", "blue", "yellow", "white", "pink", "green"]
 
 pg.init()
 
-
 class OpenSpace():
-    def __init__(self, window_size:tuple=(500, 500), background_colour:tuple=(255, 255, 255), fps:int=60):
+    def __init__(self, window_size:tuple=(500, 500), background_colour:tuple=(255, 255, 255), fps:int=30):
         self.background_colour = background_colour
         self.window_size = window_size
         self.screen = pg.display.set_mode(self.window_size)
@@ -14,94 +17,65 @@ class OpenSpace():
         self.work = True
 
     def draw_ground(self):
-        position = (
-            0, self.window_size[1]-self.window_size[1]*0.01, self.window_size[1], 5
-        )
-        pg.draw.rect(self.screen, (0,0,0), position)
+        self.screen.fill(self.background_colour)
 
 
-class Square():
-    def __init__(self, window_screen, size:int, color:tuple, cords:tuple=(0, 0)):
+
+class Object():
+    def __init__(self, window_screen, angle:int=1, cords:tuple=(0, 0), size:int=5):
         
         self.size = size
-        self.color = color
-
+        self.color = random.choice(LIST_COLORS)
+        self.angle = angle
         self.screen = window_screen
 
         # start cords
         self.cord_x, self.cord_y = cords
 
-        self.drawing = True
-
 
 
     def draw(self):
-        if self.drawing:
-            position = (
-                self.cord_x,
-                self.cord_y,
-                self.size,
-                self.size
-            )
-            pg.draw.rect(self.screen, self.color, position)
+        pg.draw.circle(self.screen.screen, self.color, (self.cord_x, self.cord_y), self.size)
+        
+        
+    def move_function(self, all_dot:int) -> int:
+        return self.cord_x * self.angle
+    
+    def move(self, all_dot:int):
+        self.cord_x += 1
+        self.cord_y = self.move_function(all_dot)
 
 
-    def move(self, speed:int, moving:str):
-
-        if moving not in ["U", "D", "R", "L"]:
-            print("\nError in name of moving. Use the ['U', 'D', 'R', 'L'].\n")
-            return
-
-        size = pg.display.get_window_size()
-
-        if moving == "U":
-            if self.cord_y > size[1]:
-                self.cord_y -= speed
-
-        elif moving == "D":
-            if self.cord_y + self.size * 1.19 < size[1]:
-                self.cord_y += speed
-
-        elif moving == "R":
-            if self.cord_x +self.size < size[0]:
-                self.cord_x += speed
-
-        elif moving == "L":
-            if self.cord_x > size[0]:
-                self.cord_x -= speed
 
 
-def start():
-    # Имя нашего окна
-    pg.display.set_caption("NEW")
+# Имя нашего окна
+pg.display.set_caption("NEW")
 
-    window = OpenSpace()
-    screen = window.screen
+window = OpenSpace()
+window.background_colour = (0, 0, 0)
+screen = window.screen
 
-    square1 = Square(screen, size=30, color=(0, 0, 0))
-    square2 = Square(screen, size=30, color=(255, 0, 0))
+number = int(input(">>> "))
 
-    while window.work:
-        # установка значения частоты обновления кадров
-        window.clock.tick(window.FPS)
+list_object = [Object(window, angle = k, cords=(window.window_size[0]//2, window.window_size[1]//2), size=10) for k in range(number)]
 
-        # Обработка события выхода
-        for event in pg.event.get():
-            if event.type == pg.QUIT:
-                sys.exit()
+while window.work:
+    # установка значения частоты обновления кадров
+    window.clock.tick(window.FPS)
 
-        square1.move(15, "D")
-        square2.move(10, "D")
+    # Обработка события выхода
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            sys.exit()
+    
+            
+    # отрисовка
+    window.draw_ground()
+    for elems in list_object:
+        elems.move(3)
+        elems.draw()
+        
+    pg.display.update()
 
+pg.quit()
 
-        # Обновление экрана
-        screen.fill(window.background_colour)
-        window.draw_ground()
-        square2.draw()
-        square1.draw()
-        pg.display.update()
-    pg.quit()
-
-
-if __name__=="__main__":
-    start()
